fa5439f2c89990e45222238cc77e071dfa31edff
warrenty <warranty6@gmail.com>
2014-09-22 13:25:47 -0700

    Prelunch progress.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,26 @@
 // YOUR CODE HERE:
+var app = {
+  init: function(){},
+  send: function(message){
+    $.ajax({
+      type: "POST",
+      url: "https://api.parse.com/1/classes/chatterbox",
+      data: JSON.stringify(message)
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      type: "GET"
+    });
+  },
+  clearMessages: function(){
+    $('#chats').html('');
+  },
+  addMessage: function(message){
+    $('#chats').append('<div></div>');
+  },
+  addRoom: function(roomName){
+    $('#roomSelect').append('<div></div>');
+  }
+};
 
